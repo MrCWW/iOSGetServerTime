874d3c72ea8e4581cacbcc61b3522a88d361547f
常旺伟 <986181064@qq.com>
2018-03-02 09:35:51 +0800

    Initial Commit

---
+++ GetNowTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetNowTime
+//
+//  Created by YunTong on 2018/3/2.
+//  Copyright © 2018年 YunTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetNowTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetNowTime
+//
+//  Created by YunTong on 2018/3/2.
+//  Copyright © 2018年 YunTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
